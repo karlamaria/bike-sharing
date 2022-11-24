72933e806bf6f665f71ecaf6ddce8056231e9185
kgarcia03 <karlamaria.garcia@yahooinc.com>
2022-11-24 22:17:15 +0000

    Add a new sub menu

--- main.py
+++ main.py
@@ -2,11 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Nov 20 14:50:17 2022
-
 @author: kgarcia03
-
 Student ID: A00304376
-
 Load dataset and Starts the user menu
 """
 
@@ -24,11 +21,12 @@ season_dict = {}
 
 def load_bike_rentals(filename):
     """
-    Load bike-sharing dataset
+    Calculate the sum of list
 
     Parameters
     ----------
-    None
+    input_list: String
+        filename that contains bike sharing data.
 
     Returns
     -------
@@ -63,15 +61,13 @@ def load_bike_rentals(filename):
         print(dir_error)
     except PermissionError as permission_error:
         print(permission_error)
-
+        
 def show_category_menu():
     """
     Analyse by Category Menu
-
     Parameters
     ----------
     None
-
     Returns
     -------
     None
@@ -91,7 +87,7 @@ def show_category_menu():
         option = input("Please Select:") 
                 
         if (option == '1'):   
-            print("Number of weathersits:", len(weathersit_dict))
+            print("Number of weathersit:", len(weathersit_dict))
             print()
             continue   
             
@@ -134,19 +130,18 @@ def show_category_menu():
             print()
             continue
 
-def show_numeric_menu():
+
+def show_common_numeric_menu(feature_list, feature_name):
     """
     Analyse by Numeric Columns Menu
-
     Parameters
     ----------
     None
-
     Returns
     -------
     None
     """
-    option = '1'
+    option = ''
     while (option != '0'):
         print("""
 [1]Number inputs
@@ -160,132 +155,147 @@ def show_numeric_menu():
 [9]InterQuartile Range
 [10]Standard Deviation
 [11]Skewness
-[12]Total Rental x Temperature Correlation
-[13]Total Rental x Wind Speed Correlation
-[14]Histogram Temperature
-[15]Histogram Rentals
-[16]Box Plot Temperature
-[17]Box Plot Rentals
-[18]Scatter Temperature vs Rentals
-[19]Scatter Windspeed vs Rentals
+[12]Histogram
+[13]Box Plot
 [0]Return to main menu""")
-        option = input("Please Select:") 
-        
+        option = input("Please Select:")
+
         if (option == '1'):
-            print("Number of inputs:", len(total_rentals))
+            print("Number of inputs:", len(feature_list))
             print()
             continue
-               
+
         if (option == '2'):
-            total_rental = stats.calc_sum(total_rentals)
+            total_rental = stats.calc_sum(feature_list)
             print("Total " + str(total_rental))
             print()
             continue
-        
+
         if (option == '3'):
-            mean_by_day = stats.calc_mean(total_rentals)
+            mean_by_day = stats.calc_mean(feature_list)
             print("Mean "  + str(mean_by_day))
             print()
             continue
-        
+
         if (option == '4'):
-            median = stats.calc_median(total_rentals)
+            median = stats.calc_median(feature_list)
             print("Median " + str(median))
             print()
             continue
-        
+
         if (option == '5'):
-            mode = stats.calc_mode(total_rentals)
+            mode = stats.calc_mode(feature_list)
             print("Mode " + str(mode))
             print()
             continue
-        
+
         if (option == '6'):
-            max_value = stats.calc_max_value(total_rentals)
+            max_value = stats.calc_max_value(feature_list)
             print("Max " + str(max_value))
             print()
             continue
-        
+
         if (option == '7'):
-            min_value = stats.calc_min_value(total_rentals)
+            min_value = stats.calc_min_value(feature_list)
             print("Min " + str(min_value))
             print()
             continue
-        
-        if (option == '8'):     
-            range_value = stats.calc_range(total_rentals)
+
+        if (option == '8'):
+            range_value = stats.calc_range(feature_list)
             print("Range " + str(range_value))
             print()
             continue
 
-        if (option == '9'):     
-            inter_quartile = stats.calc_inter_quartile(total_rentals)
+        if (option == '9'):
+            inter_quartile = stats.calc_inter_quartile(feature_list)
             print("Inter Quartile " + str(inter_quartile))
             print()
             continue
 
-        if (option == '10'):     
-            std_deviation = stats.calc_std_deviation(total_rentals)
+        if (option == '10'):
+            std_deviation = stats.calc_std_deviation(feature_list)
             print("Standard Deviation", std_deviation)
             print()
             continue
 
-        if (option == '11'):     
-            mode_skewness = stats.calc_mode_skewness(total_rentals)
+        if (option == '11'):
+            mode_skewness = stats.calc_mode_skewness(feature_list)
             print("Mode Skewness", mode_skewness)
             print()
             continue
 
-        if (option == '12'):     
+        if (option == '12'):
+            step = 1
+            if (feature_name == "Temperature"):
+                step = 200
+            visualization.plot_histogram(feature_list, feature_name, step)
+            print()
+            continue
+
+        if (option == '13'):
+            visualization.plot_box_plot(temperatures, feature_name)
+            print()
+            continue
+
+def show_numeric_menu():
+    """
+    Analyse by Numeric Columns Menu
+    Parameters
+    ----------
+    None
+    Returns
+    -------
+    None
+    """
+    option = ''
+    while (option != '0'):
+        print("""
+[1]Total Rentals Metrics
+[2]Temperature Metrics
+[3]Total Rental x Temperature Correlation
+[4]Total Rental x Wind Speed Correlation
+[5]Scatter Temperature x Rentals
+[6]Scatter Windspeed x Rentals
+[0]Return to main menu""")
+        option = input("Please Select:") 
+        
+        if (option == '1'):
+            show_common_numeric_menu(total_rentals, "Total Rental")
+            continue
+               
+        if (option == '2'):
+            show_common_numeric_menu(temperatures, "Temperature")
+            continue
+
+        if (option == '3'):
             correlation = stats.calc_correlation(total_rentals, temperatures)
             print("Total Rental x Temperature Correlation", correlation)
             print()
             continue     
 
-        if (option == '13'):   
+        if (option == '4'):
             correlation = stats.calc_correlation(total_rentals, windspeeds)
             print("Total Rental x Wind Speed Correlation", correlation)
             print()
             continue   
-        
-        if (option == '14'):   
-            visualization.plot_histogram(temperatures, "Temperature")
-            print()
-            continue 
-        
-        if (option == '15'):   
-            visualization.plot_histogram(total_rentals, "Total Rental", 200)
-            print()
-            continue 
-           
-        if (option == '16'):   
-            visualization.plot_box_plot(temperatures, "Temperature")
-            print()
-            continue 
-            
-        if (option == '17'):   
-            visualization.plot_box_plot(total_rentals, "Total Rental")
-            print()
-            continue 
-           
-        if (option == '18'):   
+
+        if (option == '5'):
             visualization.plot_scatter_plot(temperatures, total_rentals, "Temperature", "Total Rental")
             print()
             continue 
         
-        if (option == '19'):   
+        if (option == '6'):
             visualization.plot_scatter_plot(windspeeds, total_rentals, "Wind Speed", "Total Rental")
             print()
-            continue 
-        
+            continue
+
 def show_main_menu():
     """
     Shows main menu
-
     Parameters
     ----------
     None
-
     Returns
     -------
     None
@@ -310,5 +320,4 @@ def show_main_menu():
 if __name__ == "__main__":
     load_bike_rentals("bike-sharing.csv")
     show_main_menu()
-
                 
